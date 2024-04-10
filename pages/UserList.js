58c116ffc46f4b5a5188ec3b0d54211f95671a82
Jaison <jaisonschmidt@gmail.com>
2024-04-10 19:54:01 -0300

    Cria listagem de usuarios usando Axios

--- pages/UserList.js
+++ pages/UserList.js
@@ -1,12 +1,28 @@
-import { Text, Button } from "react-native";
+import axios from 'axios';
+import { useEffect } from "react";
+import { Text } from "react-native";
 
 function UserList({ navigation }) {
+    // quando carregar este componente (onLoad), acessar o endpoint (API) https://randomuser.me/api/?results=20 usando Axios
+    // e trazer os dados dos usuários para a aplicação, vamos gravar em uma variável
+    
+    useEffect(() => {
+        axios.get("https://randomuser.me/api/?results=20")
+            .then((response) => {
+                console.log(response.data.results);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <>
             <Text>Lista de usuários</Text>
-            <Button title="Ver detalhes" onPress={() => navigation.navigate("UserDetail")} />
         </>
     )
 }
 
 export default UserList;
+
+// <Button title="Ver detalhes" onPress={() => navigation.navigate("UserDetail")} />
