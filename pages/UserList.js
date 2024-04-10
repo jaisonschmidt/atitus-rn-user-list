0fa8ad823c5a33de648c6ae554b58044b32ef07d
Jaison <jaisonschmidt@gmail.com>
2024-04-10 20:04:55 -0300

    Lista os nomes dos usuários da API

--- pages/UserList.js
+++ pages/UserList.js
@@ -1,24 +1,33 @@
 import axios from 'axios';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text } from "react-native";
 
 function UserList({ navigation }) {
+    // criar um estado local para armazenar os usuários
+    const [users, setUsers] = useState([]);
+
     // quando carregar este componente (onLoad), acessar o endpoint (API) https://randomuser.me/api/?results=20 usando Axios
     // e trazer os dados dos usuários para a aplicação, vamos gravar em uma variável
-    
     useEffect(() => {
         axios.get("https://randomuser.me/api/?results=20")
             .then((response) => {
                 console.log(response.data.results);
+                setUsers(response.data.results);
             })
             .catch((error) => {
                 console.error(error);
+                // limpar a variavel users
+                setUsers([]);
             });
     }, []);
 
     return (
         <>
-            <Text>Lista de usuários</Text>
+            {users.length === 0 && <Text>Carregando usuários...</Text>}
+            
+            {users.length > 0 && users.map((user, index) => (
+                <Text key={index}>{user.name.first} {user.name.last}</Text>
+            ))}
         </>
     )
 }
